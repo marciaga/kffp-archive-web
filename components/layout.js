e1565195d041ccb68e4e4fa6561d390c7e303660
Mark Arciaga <mark.arciaga@gmail.com>
2017-05-20 22:15:54 -0700

    Add the right meta tag so app works on mobile browsers

--- components/layout.js
+++ components/layout.js
@@ -4,6 +4,9 @@ import Header from './header';
 const Layout = (props) => (
     <div>
         <Head>
+            <meta charset="utf-8" />
+            <meta http-equiv="x-ua-compatible" content="ie=edge" />
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
             <link rel="stylesheet" href='https://unpkg.com/react-md/dist/react-md.indigo-pink.min.css' />
             <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto:300,400,500' />
             <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Material+Icons' />
