7e9bf153c14a36d8a4dea297c588739b3ff18464
Mark Arciaga <mark.arciaga@gmail.com>
2020-07-03 16:41:33 -0700

    update stylesheet link      auth: false, // remove this

--- components/layout.js
+++ components/layout.js
@@ -7,7 +7,7 @@ const Layout = (props) => (
             <meta charset="utf-8" />
             <meta http-equiv="x-ua-compatible" content="ie=edge" />
             <meta name="viewport" content="width=device-width, initial-scale=1" />
-            <link rel="stylesheet" href='https://unpkg.com/react-md/dist/react-md.indigo-pink.min.css' />
+            <link rel="stylesheet" href="https://unpkg.com/react-md@1.16.1/dist/react-md.indigo-pink.min.css" />
             <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto:300,400,500' />
             <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Material+Icons' />
             <link rel="icon" href="https://s3-us-west-2.amazonaws.com/freeformpdx-images/icons/cropped-record-favicon-1.png" sizes="32x32" />
